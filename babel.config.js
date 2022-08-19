6f25a86d74eb79aef60b4523577285c5f4b4c731
irfan <72319855+irfankurtagic@users.noreply.github.com>
2022-08-19 16:11:29 +0200

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -4,4 +4,4 @@ module.exports = {
   ]
 }
 
-  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/covid_tracker/" : "/",
