f1f5559c773c74be7e697697e2df02b1bb74fdfb
irfan <72319855+irfankurtagic@users.noreply.github.com>
2022-08-19 16:16:25 +0200

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,5 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ]
-}
-
   publicPath: process.env.NODE_ENV === "production" ? "/https://irfankurtagic.github.io/covid_tracker/" : "/",
+}
